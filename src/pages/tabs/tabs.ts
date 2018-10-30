524bf2c730f823825170c0f075f91b602201da25
Adam Morgan <acm1979@gmail.com>
2018-10-30 01:19:41 -0600

    remove notifications as main nav tab, will place in more later

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { ContactsPage } from '../contacts/contacts';
 import { MorePage } from '../more/more';
-import { NotificationsPage } from '../notifications/notifications';
 import { SchedulePage } from '../schedule/schedule';
 import { TodayPage } from '../today/today';
 
@@ -14,8 +13,7 @@ export class TabsPage {
   tab1Root = TodayPage;
   tab2Root = SchedulePage;
   tab3Root = ContactsPage;
-  tab4Root = NotificationsPage;
-  tab5Root = MorePage;
+  tab4Root = MorePage;
 
   constructor() {
 
